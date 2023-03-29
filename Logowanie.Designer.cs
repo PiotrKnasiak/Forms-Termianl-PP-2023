793708e9fab0d6bdbe50fcf06ee6b4c8cbc5dfbc
Michał <mg4rd0s@gmail.com>
2023-03-29 21:45:54 +0200

    Dodanie 2 formsów (logowanie, rejestracja)
    + usunięcie testowego okienka Piotrka

---
+++ Logowanie.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Terminarz
+{
+    partial class Logowanie
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Logowanie));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.error = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(68, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Terminarz";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(77, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nazwa użytkownika";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(77, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hasło";
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_username.Location = new System.Drawing.Point(77, 187);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(215, 27);
+            this.txt_username.TabIndex = 3;
+            this.txt_username.Click += new System.EventHandler(this.txt_username_Click);
+            this.txt_username.TextChanged += new System.EventHandler(this.txt_username_TextChanged);
+            this.txt_username.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_password_TextChanged);
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(77, 254);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(215, 27);
+            this.txt_password.TabIndex = 4;
+            this.txt_password.UseSystemPasswordChar = true;
+            this.txt_password.Click += new System.EventHandler(this.txt_password_Click);
+            this.txt_password.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_password_TextChanged);
+            // 
+            // button_login
+            // 
+            this.button_login.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button_login.Location = new System.Drawing.Point(198, 300);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(94, 29);
+            this.button_login.TabIndex = 5;
+            this.button_login.Text = "Zaloguj się";
+            this.button_login.UseVisualStyleBackColor = false;
+            this.button_login.Click += new System.EventHandler(this.button1_Click);
+            this.button_login.Enter += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(77, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nie masz konta? ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Teal;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Teal;
+            this.linkLabel1.Location = new System.Drawing.Point(191, 377);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(101, 20);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Zarejestruj się";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked_1);
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.BackColor = System.Drawing.Color.Transparent;
+            this.error.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.error.ForeColor = System.Drawing.Color.Red;
+            this.error.Location = new System.Drawing.Point(77, 342);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(44, 20);
+            this.error.TabIndex = 8;
+            this.error.Text = "Error";
+            this.error.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.error.Visible = false;
+            this.error.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(809, 469);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Logowanie";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txt_username;
+        private TextBox txt_password;
+        private Button button_login;
+        private Label label4;
+        private LinkLabel linkLabel1;
+        private Label error;
+    }
+}
---
+++ Logowanie.cs
@@ -0,0 +1,157 @@
+using Microsoft.VisualBasic.Logging;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using FormsyTest2;
+
+namespace Terminarz
+{
+   public partial class Logowanie : Form
+    {
+        public Logowanie()
+        {
+            InitializeComponent();
+        }
+        //SqlConnection conn = new SqlConnection(@"Data Source=""153.19.227.34, 1433"";Initial Catalog=ProjektPP2023;Persist Security Info=True;User ID=projekt2023;Password=Projekt2023");
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string username, password, login, haslo;
+
+            username = txt_username.Text;
+            password = txt_password.Text;
+
+            Label Error = error;
+            TextBox Login = txt_username;
+            TextBox Password = txt_password;
+
+            if (username == "" && password == "")
+            {
+                Login.BackColor = Color.LightCoral;
+                Password.BackColor = Color.LightCoral;
+                Login.PlaceholderText = "Podaj nazwê u¿ytkownika";
+                Password.PlaceholderText = "Podaj has³o";
+                //Error.Text = "Podaj dane logowania";
+                //Error.Visible = true;
+                Error.Visible = false;
+            }
+            else if (username == "")
+            {
+                Login.BackColor = Color.LightCoral;
+                Password.BackColor = Color.White;
+                Login.PlaceholderText = "Podaj nazwê u¿ytkownika";
+                //Error.Text = "Podaj login";
+                //Error.Visible = true;
+                Error.Visible = false;
+            }
+            else if (password == "")
+            {
+                Login.BackColor = Color.White;
+                Password.BackColor = Color.LightCoral;
+                Password.PlaceholderText = "Podaj has³o";
+                //Error.Text = "Podaj has³o";
+                //Error.Visible = true;
+                Error.Visible = false;
+            }
+            else
+            {
+                Login.BackColor = Color.White;
+                Password.BackColor = Color.White;
+                string querry = "SELECT * FROM LoginData WHERE Login = '" + username + "' AND Password = '" + password + "'";
+                //SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dTable = new DataTable();
+                //sda.Fill(dTable);
+
+                if (dTable.Rows.Count > 0)
+                {
+                    Form form2 = new Form();
+                    form2.Show();
+                    this.Hide();
+                }
+                else if (dTable.Rows.Count == 0)
+                {
+                    Error.Text = "Login lub has³o s¹ nieprawid³owe.";
+                    Error.Visible = true;
+
+                }
+            }
+
+
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var myForm = new Rejestracja();
+            myForm.Show();
+            this.Hide();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void txt_password_TextChanged(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                button1_Click(sender, e);
+
+
+        }
+        private void txt_username_Click(object sender, EventArgs e)
+        {
+            TextBox Login = txt_username;
+            Login.BackColor = Color.White;
+        }
+
+        private void txt_password_Click(object sender, EventArgs e)
+        {
+            TextBox Haslo = txt_password;
+            Haslo.BackColor = Color.White;
+        }
+
+        private void txt_username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
